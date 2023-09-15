fe93070993412ca0a33697aeeb27ee42e92a7229
HJacksons <jacksonherberts@gmail.com>
2023-09-15 19:20:34 +0200

    code restructure

--- file_saver.py
+++ file_saver.py
@@ -2,15 +2,13 @@ import wave
 import pyaudio
 import os
 
-dire = "voice/"
-
 
 class FileSaver:
     if not os.path.exists("voice"):
         os.makedirs("voice")
 
     @staticmethod
-    def save_as_wav(frames, path=dire + "output.wav"):
+    def save_as_wav(frames, path="voice/output.wav"):
         wf = wave.open(path, 'wb')
         wf.setnchannels(2)
         wf.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
